286676d830ca2eaa8f947b5a9f67919bcf3acc16
Andrew Liu <14andrewliu@gmail.com>
2015-03-25 12:00:22 -0700

    fixed carrier_wave.rb last time

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
 	CarrierWave.configure do |config|
 		config.fog_credentials = {
 			:provider => 'AWS',
-			:aws_access_key_id => ENV['AKIAISAKCSKGEX5RQGCA']
+			:aws_access_key_id => ENV['AKIAISAKCSKGEX5RQGCA'],
 			:aws_secret_access_key => ENV['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']
 		}
 		config.fog_directory = ENV['railsproductfirstbucket']
